29138e095688a6504312e7a25ba4b941268c7e2c
SwaleNn <alwindiberlianto@gmail.com>
2024-11-12 19:18:48 +0700

    add one file

--- 8_getter_and_setter.dart
+++ 8_getter_and_setter.dart
@@ -1,22 +1,35 @@
 void main() {
-  var danang = Person("alwindi", "laki-laki", "20");
+  var danang = Person("alwindi", "laki-laki", 20, "test");
 
   print("nama : ${danang.name}");
   print("Gender : ${danang.gender}");
-  print("Age : ${danang.age}");
+  print("Age : ${danang._age}");
 
   danang.eat("Sate");
   danang.code();
+
+  print(danang._age);
+  danang.age = 18;
+  print(danang._age);
+  danang._names = "justin";
 }
 
 class Person {
   //fields
-  final String name;
-  final String gender;
-  final String age;
+  String name;
+  String
+      _names; // jika menqggunakan _(underscore) pada depan variabel maka itu akan membuat object menjadi private dan tidak akan bisa digunakan di luar class atau luar object
+  String gender;
+  int _age;
 
   // constructor
-  Person(this.name, this.gender, this.age);
+  Person(this.name, this.gender, this._age, this._names);
+
+  //getter (membuat private code supaya bisa digunakan menjadi public tanpa mengubah variabel)
+  String get personName => _names;
+
+  //setter (alternatif lain dari getter)
+  set age(int newAge) => this._age = newAge;
 
   // method
   void eat(String food) {
@@ -25,5 +38,9 @@ class Person {
 
   void code() {
     print("$name sedang ngoding");
+  }
+
+  void test() {
+    print(_names);
   }
 }
